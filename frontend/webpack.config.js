0178ffc807510503cb98ab8c7c9bb1115f91944e
박태용 <86910922+ParkTaeYonggg@users.noreply.github.com>
2022-01-04 01:59:31 +0900

    회원가입모달인증버튼전반적인틀 (#9)
    확인했음.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,10 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const mode = process.env.NODE_ENV !== 'production' ? "development" : "production";
+const mode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: mode, // 배포시 production으로
+    mode: mode ? "development" : "production", // 배포시 production으로
     name: "ToDoTogether",
 
     resolve: {
@@ -21,14 +21,15 @@ module.exports = {
                 test:/\.(js||jsx)$/,
                 loader: "babel-loader",
                 options: {
-                    presets: ["@babel/preset-env","@babel/preset-react"],
-                    plugins: mode !== "poduction" ?["react-refresh/babel",["transform-remove-console", {exclude: ["error","warn","log"]}]] : 
-                                                   [["transform-remove-console"],"react-refresh/babel"]
+                    presets: [["@babel/preset-env",{targets: {browsers: ["last 2 versions",">= 5% in KR"]}}],"@babel/preset-react"],
+                    plugins: ["react-refresh/babel",
+                             ["transform-remove-console", {exclude: ["error","warn","log"]}]
+                             ] 
                 },
             },
             {
-                test:/\.css$/,
-                use: ["css-loader","style-loader"],
+                test:/\.(css||sass||scss)$/,
+                use: ["style-loader","css-loader","sass-loader"]
             },
             {
                 test:/\.(png||jpg||gif||svg)$/,
@@ -50,8 +51,15 @@ module.exports = {
     },
     devServer: {
         port: 3000,
+        historyApiFallback: true, // 라우터 사용시 필요한 기능이다. 변경된 주소에서 새로고침해도 에러를 막아준다.
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080",
+                pathRewrite: {"/api":"/"}
+            }
+        },
         devMiddleware: {publicPath: "/"},
         static: path.resolve(__dirname, "./dist"),
-        hot: true
+        hot: mode ? true : false
     }
 }
